7c211a514829830fe8edc8dd97e9446778c0cedc
Piotr Sacharuk <suchygd@gmail.com>
2023-10-05 00:26:51 +0200

    Add endpoints and operation to perform on data in db

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,12 +1,5 @@
 from app.database.base import session_factory
 
-def select_one(query):
-    try:
-        return query.one_or_none()
-    except:
-        return None
-
-
 def select_all(query):
     try:
         return query.all()
@@ -24,3 +17,28 @@ def select_from_db(obj, session = None):
         result = None
     session.close()
     return result
+
+
+def insert_into_db(obj, session = None):
+    if session is None:
+        session = session_factory()
+    session.add(obj)
+    session.commit()
+    session.close()
+
+
+def update_db(obj, attrs_to_update, session = None):
+    if session is None:
+        session = session_factory()
+    for key in attrs_to_update:
+        setattr(obj, key, attrs_to_update[key])
+    session.commit()
+    session.close()
+
+
+def delete_from_db(obj, session = None):
+    if session is None:
+        session = session_factory()
+    session.delete(obj)
+    session.commit()
+    session.close()
--- app/model/webpage/endpoints.py
+++ app/model/webpage/endpoints.py
@@ -1,6 +1,7 @@
-from app.database.connection import select_from_db
+from app.database.connection import select_from_db, insert_into_db, update_db, delete_from_db
 from app.model.webpage.schema import Webpage
 from app.utils.webpage.page import get_page
+from app.utils.webpage.parser import parse_content
 
 from fastapi import APIRouter, HTTPException
 
@@ -11,15 +12,38 @@ def get_from_webpage(url):
     returned_url, content, error_info = get_page(url)
     if error_info:
         raise HTTPException(status_code=404, detail=error_info)
-    return {"url": returned_url, "content": content}
+    head, body, footer = parse_content(content)
+    webpage = Webpage(address=returned_url, head=head, body=body, foot=footer)
+    try:
+        # ADDRESS is unique so check if element exists. If yes then should be updated
+        get_from_db(returned_url)
+        update_db(webpage, {'head': head, 'body': body, 'foot': footer})
+    except HTTPException:
+        # if element do not exist insert it in db
+        insert_into_db(webpage)
 
-@router.get("/db")
-def get_from_db():
-    return "data from db"
+    return {"address": returned_url, "content": {
+        "head": head,
+        "body": body,
+        "foot": footer
+    }}
 
-@router.get("/db/all")
+@router.get("/db/{url:path}")
+def get_from_db(url):
+    webpages = get_all_from_db()
+    for page in webpages:
+        if page.address == url:
+            return page
+    raise HTTPException(status_code=404, detail="Not found")
+
+
+@router.get("/db-all")
 def get_all_from_db():
-    data_from_db = select_from_db(Webpage)
-    if data_from_db == None:
-        return "Still nothing in db"
-    return data_from_db
+    return select_from_db(Webpage)
+
+
+@router.delete('/db/{url:path}')
+def remove_from_db(url):
+    object_to_remove = get_from_db(url)
+    delete_from_db(object_to_remove)
+    return f"Object with {url} removed"
